0b71d593096b75b8d3bdb81676eb1989856685ec
waheng chang <wahengchang@gmail.com>
2018-10-29 11:06:38 +0800

    no message

---
+++ script/deploy.sh
@@ -0,0 +1,6 @@
+set -e
+npm version patch
+PKG_VERSION=`sh script/getPackageVersion.sh`
+docker build -t rrc-backstage-screenshoot-report:$PKG_VERSION . 
+docker tag rrc-backstage-screenshoot-report:$PKG_VERSION 354821401994.dkr.ecr.ap-southeast-1.amazonaws.com/rrc-backstage-screenshoot-report:$PKG_VERSION
+docker push 354821401994.dkr.ecr.ap-southeast-1.amazonaws.com/rrc-backstage-screenshoot-report:$PKG_VERSION
